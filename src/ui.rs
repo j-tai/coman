50e7cfb90d2b4aef71d25e1f03c931901c3a25be
James Tai <jtai@jtai.ca>
2021-07-03 14:59:17 -0700

    Refactor test case output code

--- src/ui.rs
+++ src/ui.rs
@@ -2,13 +2,15 @@ use crate::run::{RunResult, TestResult, TestStatus};
 
 mod step;
 
+use crate::step;
+
 pub fn print_n_lines(header: &str, data: &[u8], n: usize) {
     let string = String::from_utf8_lossy(data);
     let total_lines = string.lines().count();
     if total_lines == 0 {
         return;
     } else {
-        println!("--- {} ---", header);
+        eprintln!("--- {} ---", header);
     }
 
     if total_lines <= n {
@@ -17,15 +19,19 @@ pub fn print_n_lines(header: &str, data: &[u8], n: usize) {
         string
             .lines()
             .take(n - 1)
-            .for_each(|line| println!("{}", line));
-        println!("... {} more lines", total_lines - (n - 1));
+            .for_each(|line| eprintln!("{}", line));
+        eprintln!("... {} more lines", total_lines - (n - 1));
     }
 }
 
 pub fn print_run_result(result: &RunResult) {
     if !result.is_success() {
-        println!("coman: process completed with {}", result);
+        eprintln!("--- process completed with {} ---", result);
     }
+}
+
+pub fn print_test_case(case: &str) {
+    step!("TEST", "{}: ", case);
 }
 
 pub fn print_test_result(result: &TestResult) {
@@ -57,5 +63,12 @@ pub fn print_test_result(result: &TestResult) {
         eprintln!("{}.{:02} ms", millis, micros / 10);
     } else {
         eprintln!("0.{:03} ms", micros);
+    }
+
+    if !result.passed() {
+        print_n_lines("captured stderr", &result.stderr, 12);
+    }
+    if let TestStatus::Crash(run_result) = &result.status {
+        print_run_result(run_result);
     }
 }
