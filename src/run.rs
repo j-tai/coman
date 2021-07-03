7bd3b1a379794d43c4910befbde7ef55af886dff
James Tai <jtai@jtai.ca>
2021-07-03 14:12:39 -0700

    Display program output status

--- src/run.rs
+++ src/run.rs
@@ -1,4 +1,5 @@
-use std::process::Command;
+use std::fmt;
+use std::process::{Command, ExitStatus};
 
 use crate::Program;
 
@@ -17,6 +18,67 @@ mod debug;
 mod init;
 mod run;
 mod test;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum RunResult {
+    Success,
+    ExitCode(i32),
+    Signal(i32),
+    Unknown,
+}
+
+impl RunResult {
+    pub fn is_success(&self) -> bool {
+        self == &RunResult::Success
+    }
+
+    pub fn as_code(&self) -> i32 {
+        match self {
+            RunResult::Success => 0,
+            RunResult::ExitCode(code) => *code,
+            RunResult::Signal(sig) => sig | 0x80,
+            RunResult::Unknown => 255,
+        }
+    }
+}
+
+impl From<ExitStatus> for RunResult {
+    fn from(status: ExitStatus) -> Self {
+        if status.success() {
+            RunResult::Success
+        } else if let Some(code) = status.code() {
+            RunResult::ExitCode(code)
+        } else {
+            // Grab the Unix signal, if possible
+            #[cfg(unix)]
+            {
+                use std::os::unix::process::ExitStatusExt;
+                if let Some(sig) = status.signal() {
+                    return RunResult::Signal(sig);
+                }
+            }
+            RunResult::Unknown
+        }
+    }
+}
+
+impl fmt::Display for RunResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RunResult::Success => write!(f, "success"),
+            RunResult::ExitCode(code) => write!(f, "exit code {}", code),
+            // Signals listed here must be commonly seen in a contest
+            // programming environment and be portable among POSIX-compliant
+            // systems.
+            RunResult::Signal(6) => write!(f, "signal ABRT (Aborted)"),
+            RunResult::Signal(8) => write!(f, "signal FPE (Floating point exception)"),
+            RunResult::Signal(9) => write!(f, "signal KILL (Killed)"),
+            RunResult::Signal(11) => write!(f, "signal SEGV (Segmentation fault)"),
+            RunResult::Signal(sig) => write!(f, "signal {}", sig),
+            RunResult::Unknown => write!(f, "unknown exit status"),
+        }
+    }
+}
 
 fn eval_command_template(prog: &Program, temp: &[String], debug: bool) -> Command {
     let mut c = Command::new(&temp[0]);
--- src/run/debug.rs
+++ src/run/debug.rs
@@ -5,6 +5,8 @@ use anyhow::{bail, Result};
 use crate::run::eval_command_template;
 use crate::Program;
 
+use super::RunResult;
+
 /// Create a `Command` that can be used to run the program in a
 /// debugger specified in the configuration. Assumes that the
 /// program has already been compiled.
@@ -24,8 +26,8 @@ pub fn get_debug_command(prog: &Program) -> Result<Command> {
 /// put into an interactive debugger like GDB. Returns true if the
 /// debugger exited with success, or false otherwise. This assumes
 /// that the program has already been compiled in debug mode.
-pub fn debug(prog: &Program) -> Result<bool> {
+pub fn debug(prog: &Program) -> Result<RunResult> {
     let mut cmd = get_debug_command(prog)?;
     let stat = cmd.status()?;
-    Ok(stat.success())
+    Ok(stat.into())
 }
